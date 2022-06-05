e7eab00262e0499a00d4ec9a1316b955e504e025
AMPIC <vsaxena@udel.edu>
2022-06-05 02:43:35 -0400

    added filter files

---
+++ libs/PDK.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Jun  5 01:28:03 2022
+
+@author: vsaxena
+"""
+
+# Generic SiP PDK
+
+# Constants
+C = 3e8
+WL0 = 1550e-9
+
+###############################################################################
+## SOI Waveguide Type 1 - standard waveguide
+# height = 220nm
+# width  = 500nm
+###############################################################################
+
+WG1_NG     = 4.24      # Group index
+WG1_NEFF   = 2.34      # Effective index
+WG1_LOSS   = 2         # dB/cm
+
